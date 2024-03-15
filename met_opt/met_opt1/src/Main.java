d95de13831b272aed0d693c76fee05dfb4545308
Aidar042002 <71192036+Aidar042002@users.noreply.github.com>
2024-03-15 23:55:10 +0300

    add met-opt directory and add lab1 version1

---
+++ met_opt/met_opt1/src/Main.java
@@ -0,0 +1,106 @@
+public class Main {
+    public static void main(String[] args) {
+        double a=-1;
+        double b=0;
+        double e=0.003;
+
+        System.out.println(halfDivision(a,b,e));
+        System.out.println(goldenRatio(a,b,e));
+        System.out.println(chords(a,b,e));
+        System.out.println(newton(a,b,e));
+
+    }
+
+    public static double func(double x){
+        double f= Math.pow(x,4)+Math.pow(x,2)+x+1;
+        return f;
+    }
+
+    public static double getDerivative(double x, double e) {
+        return (func(x + e) - func(x - e)) / (2 * e);
+    }
+
+    public static double getSecondDerivative(double x, double e) {
+        return (func(x + e) - 2 * func(x) + func(x - e)) / Math.pow(e, 2);
+    }
+
+    public static double halfDivision(double a, double b, double e){
+        while (b-a>2*e){
+            double x1=(a+b-e)/2;
+            double x2=(a+b+e)/2;
+
+            double y1=func(x1);
+            double y2=func(x2);
+
+            if(y1>y2){
+                a=x1;
+            } else{
+                b=x2;
+            }
+
+        }
+
+        return ((a+b)/2);
+    }
+
+    public static double goldenRatio(double a, double b, double e){
+        double phi=(1+Math.sqrt(5))/2;
+        double x1=b-(b-a)/phi;
+        double x2=a+(b-a)/phi;
+
+        while ((b-a)/2>=e){
+            if(func(x1)> func(x2)){
+                a=x1;
+                x1=x2;
+                x2=b-(x1-a);
+            }else{
+                b=x2;
+                x2=x1;
+                x1=a+(b-x2);
+            }
+        }
+        return (a + b)/2;
+    }
+
+    public static double chords(double a, double b, double e) {
+        double aDer = getDerivative(a, e);
+        double bDer = getDerivative(b, e);
+
+        while (true) {
+            double x = a - (aDer / (aDer - bDer)) * (a - b);
+            double xDer = getDerivative(x, e);
+
+            if (xDer <= e) {
+                return x;
+            }
+
+            if (xDer > 0) {
+                b = x;
+                bDer = xDer;
+            } else {
+                a = x;
+                aDer = xDer;
+            }
+        }
+    }
+
+    public static double newton(double a, double b, double e) {
+        double x0 = b;
+        double x0Der = getDerivative(x0, e);
+        double x0SDer = getSecondDerivative(x0, e);
+        double x = x0 - (x0Der / x0SDer);
+
+        while (true) {
+            double xDer = getDerivative(x, e);
+
+            if (Math.abs(xDer) < e) {
+                break;
+            }
+
+            x = x - xDer / getSecondDerivative(x, e);
+        }
+
+        return x;
+    }
+
+}
